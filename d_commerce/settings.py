bd8802a72c45536600a43fd5b04dbb23a6d81ce1
urielmayo <urimayo00@gmail.com>
2023-03-02 01:45:48 -0300

    fix .env errors

--- d_commerce/settings.py
+++ d_commerce/settings.py
@@ -14,11 +14,10 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
