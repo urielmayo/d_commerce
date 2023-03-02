d3f0a834224972034ff178bc6cf654bd5c207ab0
urielmayo <urimayo00@gmail.com>
2023-03-02 01:48:34 -0300

    adding domain

--- d_commerce/settings.py
+++ d_commerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['%s' % os.getenv('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ['urielmayo.pythonanywhere.com', ]
 
 
 # Application definition
